a02856fbd70e7012f325848b7f020cef5880f221
Wang Xu <wx120986@126.com>
2015-11-04 14:29:24 +0800

    Initial Commit

---
+++ wisdomExercise/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  wisdomExercise
+//
+//  Created by wangxu on 15/11/4.
+//  Copyright © 2015年 wangxu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
